cc1d5fcb55720f0efd55a69a6e7fbe4184b752f3
Feynman <xufei@tapdata.io>
2026-09-20 09:30:23 +0800

    test(control): avoid handler-order log route regression

--- control/rest-api/src/test/java/io/tapstate/control/restapi/PipelineLogsApiTest.java
+++ control/rest-api/src/test/java/io/tapstate/control/restapi/PipelineLogsApiTest.java
@@ -218,7 +218,7 @@ class PipelineLogsApiTest {
 
         assertThat(projected)
                 .as("only the logs face projects onto this focused context")
-                .containsExactly("pipeline.logs", "pipeline.log-level");
+                .containsExactlyInAnyOrder("pipeline.logs", "pipeline.log-level");
     }
 
     /**
